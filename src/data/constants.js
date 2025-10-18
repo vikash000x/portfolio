c3e1f24a2cc5e63f52c6c41dac8694f6cbe4f4fd
vikash sinha <103680992+vikash000x@users.noreply.github.com>
2025-10-18 12:53:37 +0530

    updated bio
    resume file url changed

--- src/data/constants.js
+++ src/data/constants.js
@@ -22,7 +22,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/vikash000x",
     resume:
-      "https://drive.google.com/file/d/1sfayESheYDH7yloPSZMAk6dIiyv4ns5d/view?usp=sharing",
+      "https://drive.google.com/file/d/1CMhoazt2zWXSRs0SyEd5UWBsvTGpzi2q/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/vikash-sinha-215000259/",
     twitter: "https://x.com/vikash_sinha_",
     insta: "https://www.instagram.com/niharchandra007?igsh=Znpnc29oa2FvZHZx",
