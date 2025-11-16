53d6c284be3c60844aeacc2a0e4a1a13428f2b61
vikash sinha <103680992+vikash000x@users.noreply.github.com>
2025-11-16 23:54:15 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -18,14 +18,15 @@ export const Bio = {
     name: "Vikash Sinha",
     roles: [
       "Web Developer",
-      "Solidity Developer",
+      "full stack Developer",
       "Programmer",
+        "problem-solver"
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/vikash000x",
     resume:
-      "https://drive.google.com/file/d/1CMhoazt2zWXSRs0SyEd5UWBsvTGpzi2q/view?usp=sharing",
+      "https://drive.google.com/file/d/1ERWnezPThZOfnx9CMc-VqF2vDXqrFK1d/view?usp=drive_link",
     linkedin: "https://www.linkedin.com/in/vikash-sinha-215000259/",
     twitter: "https://x.com/vikash_sinha_",
     insta: "https://www.instagram.com/niharchandra007?igsh=Znpnc29oa2FvZHZx",
